3f9ac0ec1bd51d7f6c9318dee499d9e0c1ca3743
s570908 <songohyoung@gmail.com>
2020-08-06 19:36:52 +0900

    add console.log

--- practice.js
+++ practice.js
@@ -1,6 +1,6 @@
 var newArr2 = [10, 20, 30].map(function(index, currentValue) {
   console.log("index: ", index, "currentValue: ", currentValue); 
   console.log("this: ", this);
-  return currentValue + 5;
+  return currentValue + 6;
 });
 console.log(newArr2);
